32b454defe9dee4c846113931ffc3debb43ecd26
2017-1463 <luismsa92@gmail.com>
2025-02-12 22:26:35 -0400

    Mejoras

---
+++ veh/Domains/VehiDto/Vehiculo.cs
@@ -0,0 +1,11 @@
+﻿namespace veh.Domain.VehiDto
+{
+    public class Vehiculo
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; } = string.Empty;
+        public string Modelo { get; set; } = string.Empty;
+        public int Año { get; set; }
+        public string Color { get; set; } = string.Empty;
+    }
+}
